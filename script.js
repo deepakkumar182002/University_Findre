7af52c71e672192438238ba1c76d252662c7b359
Deepak kumar <106810862+deepakkumar182002@users.noreply.github.com>
2023-12-31 20:37:55 +0530

    .....

--- script.js
+++ script.js
@@ -1,56 +1,103 @@
+// const map = L.map('map').setView([24.5, 82], 6); // Centered around India
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; OpenStreetMap contributors'
+// }).addTo(map);
+
+// // Define the boundaries for different locations
+// const uttarPradeshBounds = [[24.5, 80], [30.5, 84]];
+// const madhyaPradeshBounds = [[21.5, 74], [26.5, 82]];
+// const delhiBounds = [[28.4, 76.8], [28.9, 77.3]];
+// const biharBounds = [[24.3, 83.5], [27.2, 88]];
+// const bengaluruBounds = [[12.8, 77.3], [13.2, 77.8]];
+// const suratBounds = [[21.1, 72.7], [21.3, 73.1]];
+// const gurgaonBounds = [[28.4, 76.9], [28.5, 77.1]];
+
+// // Event listener for map click
+// map.on('click', function (e) {
+//     // Check if the click is within the boundaries of Uttar Pradesh
+//     if (isWithinBounds(e.latlng, uttarPradeshBounds)) {
+//         window.open('uttar_pradesh.html', '_blank');
+//     }
+//     // Check if the click is within the boundaries of Madhya Pradesh
+//     else if (isWithinBounds(e.latlng, madhyaPradeshBounds)) {
+//         window.open('madhya_pradesh.html', '_blank');
+//     }
+//     // Check for other locations
+//     else if (isWithinBounds(e.latlng, delhiBounds)) {
+//         window.open('delhi.html', '_blank');
+//     }
+//     else if (isWithinBounds(e.latlng, biharBounds)) {
+//         window.open('bihar.html', '_blank');
+//     }
+//     else if (isWithinBounds(e.latlng, bengaluruBounds)) {
+//         window.open('bengaluru.html', '_blank');
+//     }
+//     else if (isWithinBounds(e.latlng, suratBounds)) {
+//         window.open('./surat.html', '_blank');
+//     }
+//     else if (isWithinBounds(e.latlng, gurgaonBounds)) {
+//         window.open('gurgaon.html', '_blank');
+//     }
+// });
+
+// // Function to check if a point is within specified bounds
+// function isWithinBounds(point, bounds) {
+//     return (
+//         point.lat >= bounds[0][0] &&
+//         point.lat <= bounds[1][0] &&
+//         point.lng >= bounds[0][1] &&
+//         point.lng <= bounds[1][1]
+//     );
+// }
+
 const map = L.map('map').setView([24.5, 82], 6); // Centered around India
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
 
-// Define the boundaries for different locations
-const uttarPradeshBounds = [[24.5, 80], [30.5, 84]];
-const madhyaPradeshBounds = [[21.5, 74], [26.5, 82]];
-const delhiBounds = [[28.4, 76.8], [28.9, 77.3]];
-const biharBounds = [[24.3, 83.5], [27.2, 88]];
-const bengaluruBounds = [[12.8, 77.3], [13.2, 77.8]];
-const suratBounds = [[21.1, 72.7], [21.3, 73.1]];
-const gurgaonBounds = [[28.4, 76.9], [28.5, 77.1]];
+        // Define the boundaries for different regions
+        const uttarPradeshBounds = [[24.5, 80], [30.5, 84]];
+        const madhyaPradeshBounds = [[21.5, 74], [26.5, 82]];
+        const delhiBounds = [[28.5, 76.5], [28.9, 77.3]];
+        const biharBounds = [[24, 83], [27, 88]];
+        const karnatakaBounds = [[12, 74], [18, 78]];
+        const gujaratBounds = [[20, 68], [24.5, 75]];
+        const newDelhiBounds = [[28.5, 76.5], [28.9, 77.3]];
+        const haryanaBounds = [[28, 75], [30, 77]];
 
-// Event listener for map click
-map.on('click', function (e) {
-    // Check if the click is within the boundaries of Uttar Pradesh
-    if (isWithinBounds(e.latlng, uttarPradeshBounds)) {
-        window.open('uttar_pradesh.html', '_blank');
-    }
-    // Check if the click is within the boundaries of Madhya Pradesh
-    else if (isWithinBounds(e.latlng, madhyaPradeshBounds)) {
-        window.open('madhya_pradesh.html', '_blank');
-    }
-    // Check for other locations
-    else if (isWithinBounds(e.latlng, delhiBounds)) {
-        window.open('delhi.html', '_blank');
-    }
-    else if (isWithinBounds(e.latlng, biharBounds)) {
-        window.open('bihar.html', '_blank');
-    }
-    else if (isWithinBounds(e.latlng, bengaluruBounds)) {
-        window.open('bengaluru.html', '_blank');
-    }
-    else if (isWithinBounds(e.latlng, suratBounds)) {
-        window.open('./Homepage.html', '_blank');
-    }
-    else if (isWithinBounds(e.latlng, gurgaonBounds)) {
-        window.open('gurgaon.html', '_blank');
-    }
-});
+        // Event listener for map click
+        map.on('click', function (e) {
+            // Check if the click is within the boundaries of different regions
+            if (isWithinBounds(e.latlng, uttarPradeshBounds)) {
+                window.open('uttar_pradesh.html', '_blank');
+            } else if (isWithinBounds(e.latlng, madhyaPradeshBounds)) {
+                window.open('madhya_pradesh.html', '_blank');
+            } else if (isWithinBounds(e.latlng, delhiBounds)) {
+                window.open('delhi.html', '_blank');
+            } else if (isWithinBounds(e.latlng, biharBounds)) {
+                window.open('bihar.html', '_blank');
+            } else if (isWithinBounds(e.latlng, karnatakaBounds)) {
+                window.open('karnataka.html', '_blank');
+            } else if (isWithinBounds(e.latlng, gujaratBounds)) {
+                window.open('gujarat.html', '_blank');
+            } else if (isWithinBounds(e.latlng, newDelhiBounds)) {
+                window.open('new_delhi.html', '_blank');
+            } else if (isWithinBounds(e.latlng, haryanaBounds)) {
+                window.open('haryana.html', '_blank');
+            }
+        });
 
-// Function to check if a point is within specified bounds
-function isWithinBounds(point, bounds) {
-    return (
-        point.lat >= bounds[0][0] &&
-        point.lat <= bounds[1][0] &&
-        point.lng >= bounds[0][1] &&
-        point.lng <= bounds[1][1]
-    );
-}
-
+        // Function to check if a point is within specified bounds
+        function isWithinBounds(point, bounds) {
+            return (
+                point.lat >= bounds[0][0] &&
+                point.lat <= bounds[1][0] &&
+                point.lng >= bounds[0][1] &&
+                point.lng <= bounds[1][1]
+            );
+        }
 
 
 
